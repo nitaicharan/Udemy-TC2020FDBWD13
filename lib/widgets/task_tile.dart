033e5d21d10d0123d48abb29ef732991f907b4c5
Nitai Charan <nitaicharan@gmail.com>
2020-01-20 08:31:12 -0300

    200. What is State and How do we Manage it?

--- lib/widgets/task_tile.dart
+++ lib/widgets/task_tile.dart
@@ -1,13 +1,42 @@
 import 'package:flutter/material.dart';
 
+class TaskTile extends StatefulWidget {
+  @override
+  _TaskTileState createState() => _TaskTileState();
+}
 
-class TaskTile extends StatelessWidget {
+class _TaskTileState extends State<TaskTile> {
+  bool isChecked = false;
 
   @override
   Widget build(BuildContext context) {
     return ListTile(
-      title: Text('This is a task.'),
-      trailing: Checkbox(value: false),
+      title: Text(
+        'This is a task.',
+        style: TextStyle(
+          decoration: this.isChecked ? TextDecoration.lineThrough : null,
+        ),
+      ),
+      trailing: TaskCheckbox(
+        checkboxState: isChecked,
+        toggleCheckboxState: (checkboxSate) =>
+            setState(() => isChecked = checkboxSate),
+      ),
+    );
+  }
+}
+
+class TaskCheckbox extends StatelessWidget {
+  TaskCheckbox({this.checkboxState, this.toggleCheckboxState});
+  final bool checkboxState;
+  final Function toggleCheckboxState;
+
+  @override
+  Widget build(BuildContext context) {
+    return Checkbox(
+      activeColor: Colors.lightBlueAccent,
+      value: this.checkboxState,
+      onChanged: this.toggleCheckboxState,
     );
   }
 }
